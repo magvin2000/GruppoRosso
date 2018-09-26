31e05665a38429a2894c142021509fe4ec7a8ae3
pesa2000 <36411479+pesa2000@users.noreply.github.com>
2018-09-26 10:20:22 +0200

    Pesa

---
+++ Menu.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="Gruppo_Rosso/css/styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Mediaworld</a>
+    </div>
+    <ul class="nav navbar-nav" id="nav">
+        <li>
+    </ul>
+  </div>
+</nav>
+</head>
+<body>
+</body>
+<!--<button type='submit' class='btn btn-primary' name='LogOut' data-toggle='modal'  onclick='LogOut()'><span class='glyphicon glyphicon-log-out'></span></button>    <div class="container" style="align-right" id="mostra"> -->                     
+<br>
+<h2 align='center'>
+<a href="TabellaMilano.php"> Giacenze Milano </a>
+<br> <br>
+<a href="TabellaRimini.php"> Giacenze Rimini</a>
+<br> <br>
+<a href="TabellaMaggiorCosto.php"> Maggior Costo</a>
+</h2>
+<p id="prova" class="hidden"></p>
