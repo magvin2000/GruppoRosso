e511768232cac36b686c47b13d2e255b1355590f
pesa2000 <36411479+pesa2000@users.noreply.github.com>
2018-09-27 12:48:04 +0200

    3

--- Select.php
+++ Select.php
@@ -1,11 +1,25 @@
 <?php
-include("config.php");
-    $tabella=$_GET['tabella'];
+    include("config.php");
+    $tabella=$_GET['tabella']; 
+    $sql = "SELECT COUNT(*) FROM ". $tabella;
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $tot_records = $stmt->fetchColumn();
     $sql = "SHOW COLUMNS FROM ". $dbname . "." . $tabella;
     $stmt = $db->prepare($sql);
     $stmt->execute();
     echo "<thead><tr>";
     $i=0;
+    $perpage = 10;
+    $page = 1;
+    if(isset($_GET['page']))
+    {
+        $page = filter_var($_GET['page'],FILTER_SANITIZE_NUMBER_INT);
+    }
+    $tot_pagine = ceil($tot_records/$perpage);
+    $pagina_corrente = $page;
+    $primo = ($pagina_corrente-1)*$perpage;
+
     while($field = $stmt->fetch(PDO::FETCH_ASSOC))
     {        
         $campo=$field['Field'];
@@ -13,12 +27,16 @@ include("config.php");
         $i++;
     }
     echo "</tr></thead>";
-    $sql = "SELECT * FROM " . $dbname . "."  . $tabella ;
+    $sql = "SELECT * FROM ". $dbname . "." . $tabella . " LIMIT " . $primo . ',' . $perpage . ' ';
     $stmt1 = $db->prepare($sql);
     $stmt1->execute();
-    
+    if(isset($_GET['page']))
+    {
+        $page = filter_var($_GET['page'],FILTER_SANITIZE_NUMBER_INT);
+    }
     while($row = $stmt1->fetch(PDO::FETCH_ASSOC))
     {
+        //$j++;
         $sql = "SHOW COLUMNS FROM " . $dbname . "."  . $tabella;
         $stmt = $db->prepare($sql);
         $stmt->execute();
@@ -35,4 +53,25 @@ include("config.php");
             $i++;
         }
         echo "</tr>";
+        
     }
+        echo '<tr><td colspan="7"><nav><ul class="pagination">';
+        for($i=1; $i<=$tot_pagine; $i++)
+    {
+        echo '<li><a href="?Select.php?page='.$i.'">'.$i.'</a></li>';
+    }
+        echo "</ul></nav></td></tr>";
+        echo '</table>';
+   /* $tot_pagine = ceil($j/$perpage);
+    $pagina_corrente = $page;
+    $primo = ($pagina_corrente-1)*$perpage;
+    $sql = 'SELECT * FROM utenti ORDER BY id DESC LIMIT '.$primo.','.$perpage.' ';*/
+
+    /*$out.='<tr><td colspan="7"><nav><ul class="pagination">';
+    for($i=1; $i<=$tot_pagine; $i++)
+        {
+        $out .='<li><a href="?page='.$i.'">'.$i.'</a></li>';
+        }
+    $out .= "</ul></nav></td></tr>";
+    $out.='</table>';
+    return($out);*/
