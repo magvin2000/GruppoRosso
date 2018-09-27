7e574923af07b95700a00473c19ecf1a5835f8f5
magvin2000 <36411497+magvin2000@users.noreply.github.com>
2018-09-27 16:36:09 +0200

    v1.0.100

--- TabellaRimini.php
+++ TabellaRimini.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="Gruppo_Rosso/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
     <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
